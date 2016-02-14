993bf56dbc6f3840a40177c2a4c4c162da0ac6d8
Fabian Pimminger <fabianpimminger@Fabians-Air.lan>
2016-02-14 19:11:33 +0100

    fix check of interface implementation of url and path generators

--- src/PathGenerators/PathGeneratorFactory.php
+++ src/PathGenerators/PathGeneratorFactory.php
@@ -7,7 +7,7 @@ class PathGeneratorFactory
     {
         $pathGeneratorClass = BasePathGenerator::class;
         $customPathClass = config('cargo.custom_path_generator_class');
-        if ($customPathClass && class_exists($customPathClass) && is_subclass_of($customPathClass, PathGenerator::class)) {
+        if ($customPathClass && class_exists($customPathClass) && class_implements($customPathClass, PathGeneratorInterface::class)) {
             $pathGeneratorClass = $customPathClass;
         }
         return app($pathGeneratorClass);
--- src/UrlGenerators/UrlGeneratorFactory.php
+++ src/UrlGenerators/UrlGeneratorFactory.php
@@ -7,7 +7,7 @@ class UrlGeneratorFactory
     {
         $urlGeneratorClass = LocalUrlGenerator::class;
         $customUrlClass = config('cargo.custom_url_generator_class');
-        if ($customUrlClass && class_exists($customUrlClass) && is_subclass_of($customUrlClass, UrlGenerator::class)) {
+        if ($customUrlClass && class_exists($customUrlClass) && class_implements($customUrlClass, UrlGeneratorInterface::class)) {
             $urlGeneratorClass = $customUrlClass;
         }
         return app($urlGeneratorClass);
